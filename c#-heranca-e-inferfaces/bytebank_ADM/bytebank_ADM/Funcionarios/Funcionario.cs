4f9be34976ec9c7082400a1b8d90298ea4c75174
Filipe <fgallo_dev@hotmail.com>
2022-09-08 13:23:57 -0300

    Organizando o projeto

---
+++ c#-heranca-e-inferfaces/bytebank_ADM/bytebank_ADM/Funcionarios/Funcionario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank_ADM.Funcionarios
+{
+    public class Funcionario
+    {
+        public Funcionario(string nome, string cpf, double salario)
+        {
+            Nome = nome;
+            Cpf = cpf;
+            Salario = salario;
+        }
+
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public double Salario { get; set; }
+
+        public double Bonificacao
+        {
+            get => Salario *= 0.1;
+        }
+    }
+}
--- c#-heranca-e-inferfaces/bytebank_ADM/bytebank_ADM/Program.cs
+++ c#-heranca-e-inferfaces/bytebank_ADM/bytebank_ADM/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using bytebank_ADM.Funcionarios;
+
+Console.WriteLine("Bem-vindo ao ByteBank Administração!\n");
+
+Funcionario funcionario1 = new("Filipe Gallo", "2938128320", 1908.0);
+
+Console.WriteLine($"Bonificação: R$ {funcionario1.Bonificacao}");
